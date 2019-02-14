4690bdf471ece63fcfbe04f2913fd4e51081a8d6
mpissanos <mpissano@gmail.com>
2019-02-14 15:17:29 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// use this to copy code snippets or use your browser's console
+// use this to copy code snippets or use your browser's consolelet sally = { name: 'Sally' };
+ 
+function greet(customer) {
+  console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally);
